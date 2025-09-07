9cb266130e2f255ac79610d320b2b313bf3f9aeb
syam-bukkuru <syambukkuru@gmail.com>
2025-09-07 10:28:56 +0530

    Frontend config Vite + React + Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-purple-600 underline">
+        Fashion Store Ready! 👗👕
+      </h1>
+    </div>
+  )
+}
